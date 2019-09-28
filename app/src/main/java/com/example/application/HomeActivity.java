1bfda55cfec4a5836b0b266a50d0d10c6d43e549
Sanskriti Lamsal <sanskritilamsal@hst000148809um.wifi02.olemiss.edu>
2019-09-28 14:30:14 -0500

    couple changes to css

--- app/src/main/java/com/example/application/HomeActivity.java
+++ app/src/main/java/com/example/application/HomeActivity.java
@@ -8,17 +8,28 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
 
     Button btnLogout;
-    FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null) {
+            String email = firebaseUser.getUid();
+        }
+
+
 
         btnLogout = findViewById(R.id.logout);
 
@@ -30,5 +41,10 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intToLogin);
             }
         });
+
+
+
+
+
     }
 }
