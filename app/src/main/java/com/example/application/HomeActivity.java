a43c207755c61b18194aa61113872184a01531f5
Sanskriti Lamsal <slamsal@go.olemiss.edu>
2019-09-28 16:52:19 -0500

    plaid authentication

--- app/src/main/java/com/example/application/HomeActivity.java
+++ app/src/main/java/com/example/application/HomeActivity.java
@@ -3,12 +3,19 @@ package com.example.application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.HashMap;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -21,30 +28,168 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        // Initialize Link
+        HashMap<String, String> linkInitializeOptions = new HashMap<String,String>();
+        linkInitializeOptions.put("key", "b9d2de60a7a88f5b4d3125f0acb1f3");
+        linkInitializeOptions.put("product", "auth");
+        linkInitializeOptions.put("apiVersion", "v2"); // set this to "v1" if using the legacy Plaid API
+        linkInitializeOptions.put("env", "sandbox");
+        linkInitializeOptions.put("clientName", "Test App");
+        linkInitializeOptions.put("selectAccount", "true");
+        linkInitializeOptions.put("webhook", "http://requestb.in");
+        linkInitializeOptions.put("baseUrl", "https://cdn.plaid.com/link/v2/stable/link.html");
 
-        mAuth = FirebaseAuth.getInstance();
-        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        // Optionally specify email address and legal name for microdeposits. http://plaid.com/docs#auth
+        linkInitializeOptions.put("userEmailAddress", "USER_EMAIL_ADDRESS");
+        linkInitializeOptions.put("userLegalName", "USER_LEGAL_NAME");
 
-        if (firebaseUser != null) {
-            String email = firebaseUser.getUid();
-        }
+        // If initializing Link in PATCH / update mode, also provide the public_token
+        // linkInitializeOptions.put("token", "PUBLIC_TOKEN")
 
+        // Generate the Link initialization URL based off of the configuration options.
+        final Uri linkInitializationUrl = generateLinkInitializationUrl(linkInitializeOptions);
 
+        // Modify Webview settings - all of these settings may not be applicable
+        // or necessary for your integration.
+        final WebView plaidLinkWebview = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = plaidLinkWebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        WebView.setWebContentsDebuggingEnabled(true);
 
-        btnLogout = findViewById(R.id.logout);
+        // Initialize Link by loading the Link initialization URL in the Webview
+        plaidLinkWebview.loadUrl(linkInitializationUrl.toString());
 
-        btnLogout.setOnClickListener(new View.OnClickListener() {
+        // Override the Webview's handler for redirects
+        // Link communicates success and failure (analogous to the web's onSuccess and onExit
+        // callbacks) via redirects.
+        plaidLinkWebview.setWebViewClient(new WebViewClient() {
             @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intToLogin = new Intent(HomeActivity.this, MainActivity.class);
-                startActivity(intToLogin);
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // Parse the URL to determine if it's a special Plaid Link redirect or a request
+                // for a standard URL (typically a forgotten password or account not setup link).
+                // Handle Plaid Link redirects and open traditional pages directly in the  user's
+                // preferred browser.
+                Uri parsedUri = Uri.parse(url);
+                if (parsedUri.getScheme().equals("plaidlink")) {
+                    String action = parsedUri.getHost();
+                    HashMap<String, String> linkData = parseLinkUriData(parsedUri);
+
+                    if (action.equals("connected")) {
+                        // User successfully linked
+                        Log.d("Public token: ", linkData.get("public_token"));
+                        Log.d("Account ID: ", linkData.get("account_id"));
+                        Log.d("Account name: ", linkData.get("account_name"));
+                        Log.d("Institution id: ", linkData.get("institution_id"));
+                        Log.d("Institution name: ", linkData.get("institution_name"));
+
+                        // Reload Link in the Webview
+                        // You will likely want to transition the view at this point.
+                        plaidLinkWebview.loadUrl(linkInitializationUrl.toString());
+                    } else if (action.equals("exit")) {
+                        // User exited
+                        // linkData may contain information about the user's status in the Link flow,
+                        // the institution selected, information about any error encountered,
+                        // and relevant API request IDs.
+                        Log.d("User status in flow: ", linkData.get("status"));
+                        // The request ID keys may or may not exist depending on when the user exited
+                        // the Link flow.
+                        Log.d("Link request ID: ", linkData.get("link_request_id"));
+                        Log.d("API request ID: ", linkData.get("plaid_api_request_id"));
+
+                        // Reload Link in the Webview
+                        // You will likely want to transition the view at this point.
+                        plaidLinkWebview.loadUrl(linkInitializationUrl.toString());
+                    } else if (action.equals("event")) {
+                        // The event action is fired as the user moves through the Link flow
+                        Log.d("Event name: ", linkData.get("event_name"));
+                    } else {
+                        Log.d("Link action detected: ", action);
+                    }
+                    // Override URL loading
+                    return true;
+                } else if (parsedUri.getScheme().equals("https") ||
+                        parsedUri.getScheme().equals("http")) {
+                    // Open in browser - this is most  typically for 'account locked' or
+                    // 'forgotten password' redirects
+                    view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                    // Override URL loading
+                    return true;
+                } else {
+                    // Unknown case - do not override URL loading
+                    return false;
+                }
             }
         });
-
-
-
-
-
     }
+
+    // Generate a Link initialization URL based on a set of configuration options
+    public Uri generateLinkInitializationUrl(HashMap<String,String>linkOptions) {
+        Uri.Builder builder = Uri.parse(linkOptions.get("baseUrl"))
+                .buildUpon()
+                .appendQueryParameter("isWebview", "true")
+                .appendQueryParameter("isMobile", "true");
+        for (String key : linkOptions.keySet()) {
+            if (!key.equals("baseUrl")) {
+                builder.appendQueryParameter(key, linkOptions.get(key));
+            }
+        }
+        return builder.build();
+    }
+
+    // Parse a Link redirect URL querystring into a HashMap for easy manipulation and access
+    public HashMap<String,String> parseLinkUriData(Uri linkUri) {
+        HashMap<String,String> linkData = new HashMap<String,String>();
+        for(String key : linkUri.getQueryParameterNames()) {
+            linkData.put(key, linkUri.getQueryParameter(key));
+        }
+        return linkData;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        mAuth = FirebaseAuth.getInstance();
+//        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+//
+//        if (firebaseUser != null) {
+//            String email = firebaseUser.getUid();
+//            Strin
+//        }
+//
+//
+//
+//        btnLogout = findViewById(R.id.logout);
+//
+//        btnLogout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FirebaseAuth.getInstance().signOut();
+//                Intent intToLogin = new Intent(HomeActivity.this, MainActivity.class);
+//                startActivity(intToLogin);
+//            }
+//        });
+
+
+
+
+
 }
+
